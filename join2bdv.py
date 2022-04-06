edd471b6894117b4902eda2b665c39f6f6d947ef
Martin Schorb <martin.schorb@embl.de>
2022-04-06 14:12:02 +0200

    parallelize

--- join2bdv.py
+++ join2bdv.py
@@ -11,7 +11,7 @@ import mrcfile as mrc
 import sys
 import os
 import mobie
-
+from multiprocessing import Pool
 
 
 target='slurm'
@@ -37,10 +37,9 @@ with open(fileslist,'r') as f:
 
 idx = 0
 
-for file in joinlist:
+def mobieconvert(file):
+    file = os.path.abspath(os.path.join(indir, file))
 
-    file = os.path.abspath(os.path.join(indir,file))
-    
     base = os.path.basename(file).split('_join')[0]
 
     # if os.path.exists(os.path.join(outdir,re.sub('/','_',base)+'.ome.zarr')):
@@ -58,16 +57,16 @@ for file in joinlist:
     # else:
     #     time.sleep(submit_interval)
 
-    print('converting '+base+' into MoBIE format.')
-    
+    print('converting ' + base + ' into MoBIE format.')
+
     # get pixel size
 
-    mfile = mrc.mmap(file,permissive = 'True')
-    tomopx = mfile.voxel_size.x / 10000 # in um
-    del(mfile)
-    resolution=[tomopx]*3
+    mfile = mrc.mmap(file, permissive='True')
+    tomopx = mfile.voxel_size.x / 10000  # in um
+    del (mfile)
+    resolution = [tomopx] * 3
 
-    infile=file
+    infile = file
 
     mobie.add_image(infile,
                     "data",
@@ -77,47 +76,12 @@ for file in joinlist:
                     resolution,
                     downscale_factors,
                     chunks,
-                    file_format = "ome.zarr",
+                    file_format="ome.zarr",
                     target=target
-                        )
-    # mat=np.eye(4)*tomopx
-    # mat[2,2] = tomopx*zstretch
-    # mat[3,3] = 1
-    
-    # tf_tr = tf.matrix_to_transformation(mat).tolist()
-
-    # setup_id = 0
-
-    # view=dict()
-    #
-    # view['resolution'] = [tomopx,tomopx,tomopx*zstretch]
-    # view['setup_id'] = setup_id
-    # view['setup_name'] = os.path.basename(base)
-    #
-    # view['OriginalFile'] = file
-    #
-    # view['trafo'] = dict()
-    # view['trafo']['Scaling'] = tf_tr
-    #
-    # view['attributes'] = dict()
-
-    # view['attributes']['displaysettings'] = dict({'id':setup_id,'color':bdv.colors['W'],'isset':'true'})
-    # view['attributes']['displaysettings']['Projection_Mode'] = 'Average'
-
-    # data = mfile.data
+                    )
 
 
-    # # check if volume is rotated
-    # if data.shape[0]/data.shape[1]>5:
-    #     data = np.swapaxes(data,0,1)
+with Pool(5) as p:
+    p.map(mobieconvert, joinlist)
 
-    # data0 = np.swapaxes(data,0,2)
-    # data1 = np.fliplr(data0)
-    # data2 = np.swapaxes(data1,0,2)
 
-   # print(outfile)
-
-# bdv.write_bdv(outfile, file, view, outf='.n5', blow_2d=zstretch, downscale_factors=downscale_factors, cluster=cluster,
-#               infile=file, chunks=chunks)
-
-del(view)
